b1c3d92fabc3305c4e1dc58c9d2dd4c570ad0688
Marie <Mariepolmontiel@gmail.com>
2020-02-24 13:08:23 +0100

    server reform

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
-const secret = 'jesuislaplusbelle';
-
-
 // /index.js
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 //import the models and routes files  :
-require('./models/Profile');
-require('./models/User');
+const User = require('./models/User');
+
+
+//MIDDLEWARE
+const withAuth = require('./middleware.js');
+/* ---- */
+
+const secret = 'jesuislaplusbelle';
+//
 
 
 
+
+
+
+//IMPORT MODELS
+const app = express();
+
+
+mongoose.Promise = global.Promise;
+mongoose.set('bufferCommands', false);
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+
+const uri = "mongodb+srv://Clayclay:ezmcpol@worldpalcluster-bccal.mongodb.net/api?retryWrites=true&w=majority";
+
+mongoose.connect(uri, options).catch(err => console.log(err.reason));
+
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+/*
 const mongo_uri = 'mongodb://localhost/react-auth';
 mongoose.connect(mongo_uri, function(err) {
   if (err) {
@@ -21,39 +50,16 @@ mongoose.connect(mongo_uri, function(err) {
     console.log(`Successfully connected to ${mongo_uri}`);
   }
 });
+*/
 
-
-
-// IMPORT MODELS
-
-const app = express();
-
-mongoose.Promise = global.Promise;
-
-mongoose.set('bufferCommands', false);
-
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-
-
-//MIDDLEWARE
-const uri = "mongodb+srv://Clayclay:ezmcpol@worldpalcluster-bccal.mongodb.net/api?retryWrites=true&w=majority";
-
-mongoose.connect(uri, options).catch(err => console.log(err.reason));
-
-app.use(bodyParser.json());
 
 //IMPORT ROUTES
-//Router Middleware
-
-
 require('./routes/Routes')(app);
+/*---*/
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
   const path = require('path');
   app.get('*', (req,res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
@@ -61,6 +67,7 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 
+app.use(express.static(path.join(__dirname, 'public')));
  
 //PORT
 const PORT = process.env.PORT || 5000;
