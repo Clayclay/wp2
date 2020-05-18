b918458764e36c137a662b5ab15fadf9ee92460b
Marie <Mariepolmontiel@gmail.com>
2020-05-18 15:00:51 +0200

    Preview fctionne

--- client/src/hooks/Upload.js
+++ client/src/hooks/Upload.js
@@ -8,12 +8,22 @@ export const Upload = () => {
   const id = authState.user._id;
 
   const [data, setData] = useState();
+  const [img, setImg] = useState(null);
 
   const handleChange = e => {
     console.log(e.target.files[0])
       setData(
           e.target.files[0] 
          );
+
+         if (e.target.files.length > 0) {
+          const reader = new FileReader();
+      
+          reader.addEventListener("load", () => {
+            setImg(reader.result);
+          });
+          reader.readAsDataURL(e.target.files[0]);
+        }
   };
  const HandleSubmit = (e) =>{
 
@@ -29,10 +39,21 @@ export const Upload = () => {
 
  }
 
+function Preview({ img }) {
+  console.log(img);
+
+  if (!img) {
+    return null;
+  }
+
+  return <img src={img} alt="" />;
+}
+
+
 return (
   <div>
       <form onSubmit={HandleSubmit}>
-       
+        <Preview img={img} />
         <input
           type="file"
           name="avatar"
