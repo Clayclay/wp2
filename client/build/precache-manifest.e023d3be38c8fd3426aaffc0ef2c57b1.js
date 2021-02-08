c5c7f98f333e3232e55804e9d576f407ab35a44e
Marie <Mariepolmontiel@gmail.com>
2021-02-08 13:03:15 +0100

    First Push

---
+++ client/build/precache-manifest.e023d3be38c8fd3426aaffc0ef2c57b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be5ecf7daf552fa8adef0cd3140d495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d1dd968ce648bd33f1d",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "80692a89cf0ca4500974",
+    "url": "/static/css/main.fa42d0ec.chunk.css"
+  },
+  {
+    "revision": "9d1dd968ce648bd33f1d",
+    "url": "/static/js/2.fc04ec80.chunk.js"
+  },
+  {
+    "revision": "b9bea2aba969600c95e6d34fab62876c",
+    "url": "/static/js/2.fc04ec80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80692a89cf0ca4500974",
+    "url": "/static/js/main.112be1ee.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
