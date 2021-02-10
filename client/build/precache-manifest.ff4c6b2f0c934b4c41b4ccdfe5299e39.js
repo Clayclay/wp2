87f47640752e188ea53464d73e006996f1a66a55
Marie <Mariepolmontiel@gmail.com>
2021-02-10 11:08:40 +0100

    build fixed ?

---
+++ client/build/precache-manifest.ff4c6b2f0c934b4c41b4ccdfe5299e39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8885cb344bc2611fb1f88e5305259fc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fcad517347783a4f9ed",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "da3c3a4e555490be9102",
+    "url": "/static/css/main.6e5b0b33.chunk.css"
+  },
+  {
+    "revision": "0fcad517347783a4f9ed",
+    "url": "/static/js/2.3042076a.chunk.js"
+  },
+  {
+    "revision": "b9bea2aba969600c95e6d34fab62876c",
+    "url": "/static/js/2.3042076a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3c3a4e555490be9102",
+    "url": "/static/js/main.93b58b18.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
