4821180b62095444868dd5484e7653b28c37711d
Marie <Mariepolmontiel@gmail.com>
2021-02-13 11:35:30 +0100

    strange wp2

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
  
 //SOCKET.IO 
 
