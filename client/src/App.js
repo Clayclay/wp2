40cf2e9ac0ee37c3d87bf556d62b5ee4b3ee6584
Marie <Mariepolmontiel@gmail.com>
2021-03-31 14:34:51 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import React, {useReducer, createContext} from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import AuthReducer from './store/reducers/auth_reducer';
-
 import withAuth from './withAuth';
 import Home from './hooks/Home/Home';
 import Login from './hooks/Login/Login';
@@ -20,10 +18,10 @@ import EditAlbum  from './hooks/EditUser/Albums/EditAlbum';
 import Album from "./hooks/Album";
 import Appbar from "./hooks/Appbar/Appbar";
 import ResetPassword from "./hooks/ResetPswd/ResetPassword";
-
 import {  ThemeProvider } from '@material-ui/core/styles';
 import theme from  './theme';
 
+import Match from './hooks/Match/Match';
 
 export const initialState = {
   is_authenticated: false,
@@ -63,12 +61,12 @@ function App()    {
       <Route path="/resetpassword" ><ResetPassword /></Route>
       <Route path="/register" exact>{ ! state.is_authenticated ? <Register  />:<Home  />}</Route>
       <Route exact path="/">{ ! state.is_authenticated ? <Login />:<Home  />}</Route>
-      
       <Route path="/edit">{ ! state.is_authenticated ? <Login />:<Edit  />} </Route>
-
       <Route path="/createAlbum">{ ! state.is_authenticated ? <Login />:<CreateAlbum  />} </Route>
       <Route exact path="/editAlbum/:id">{ ! state.is_authenticated ? <Login />:<EditAlbum  />} </Route>
       <Route exact path="/album/:id" >{ ! state.is_authenticated ? <Login />:<Album />}</Route>
+
+      <Route exact path="/match" >{ ! state.is_authenticated ? <Login />:<Match />}</Route>
 
       <Route exact path="/users" >{ ! state.is_authenticated ? <Login />:<Users />}</Route>
       <Route exact path="/user/:id">{ ! state.is_authenticated ? <Login />:<Profile  />}</Route>
