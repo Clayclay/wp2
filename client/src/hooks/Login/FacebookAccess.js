0e6a040fa155a3f4b481c22dc36070daabab9fcb
Marie <Mariepolmontiel@gmail.com>
2021-04-29 20:10:25 +0200

    test fb.login

--- client/src/hooks/Login/FacebookAccess.js
+++ client/src/hooks/Login/FacebookAccess.js
@@ -6,25 +6,60 @@ import useInitFacebookSDK from './useInitFacebookSDK';
 const FacebookAccess = ( ) => {
 
     const isFbSDKInitialized = useInitFacebookSDK();
-    const[fbLoginStatus,setFbLoginStatus]=useState();
+    const[loginState,setLoginState]=useState();
     //const[fbLoginState,setFbLoginState]=useState();
-
-
+    const [fbUserAccessToken, setFbUserAccessToken] = useState();
+   /* FB.getLoginStatus(function(response) {
+        statusChangeCallback(response) */
+        
     useEffect(()=>{
         if(isFbSDKInitialized){
             window.FB.getLoginStatus((response)=>{
-                setFbLoginStatus(response)
+                setLoginState(response)
             });
         }
-    },[isFbSDKInitialized]);
-   
 
-console.log('fbLogin', fbLoginStatus,'isfbinitialized',isFbSDKInitialized)
+        window.FB.login(function(response) {
+            if (response.status === 'connected') {
+             console.log('Welcome!  Fetching your information.... ');
+             window.FB.api('/me', function(response) {
+               console.log('Good to see you, ' + response.name + '.');
+             });
+            } else {
+             console.log('User cancelled login or did not fully authorize.');
+            }
+        });
+        
+    },[isFbSDKInitialized]);
+
+console.log(
+'fbLogin', loginState,
+'isfbinitialized',isFbSDKInitialized,
+'accesstoken',fbUserAccessToken
+)
+
+
+
+   
+//callback change only if one input change
+    const logInToFB = useCallback(() => {
+        window.FB.login((response) => {
+          setFbUserAccessToken(response.authResponse.accessToken);
+        });
+      }, []);
+
+
 
 return (
-<div>
-    FacebookAccess
-</div>
+
+<div class="fb-login-button" 
+data-width="" 
+data-size="large" 
+data-button-type="login_with" 
+data-layout="default" 
+data-auto-logout-link="true" 
+data-use-continue-as="false"></div>
+
 );
 
 }
