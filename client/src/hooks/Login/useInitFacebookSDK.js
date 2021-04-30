fb8269770f16eec47114be765c58daed56f6d046
Marie <Mariepolmontiel@gmail.com>
2021-04-30 15:31:25 +0200

    test useinit

--- client/src/hooks/Login/useInitFacebookSDK.js
+++ client/src/hooks/Login/useInitFacebookSDK.js
@@ -1,40 +1,23 @@
 import React from 'react';
 
-
-// Injects the Facebook SDK into the page
-const injectFbSDKScript = () => {
-  (function (d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-      return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, "script", "facebook-jssdk");
-};
-
 export const useInitFacebookSDK = () => {
-
-    const fcbAppId = '281271229627551' // process.env.FACEBOOK_APP_ID
-    const [isInitialized,setIsInitialized]=React.useState(false)
-
-    window.fbAsyncInit = function () {
-        window.FB.init({
-          appId      : fcbAppId,
-          cookie     : true,
-          xfbml      : true,
-          version    : 'v10.0'
-        });
-        window.FB.AppEvents.logPageView();
-        setIsInitialized(true);
-    };
-
-    injectFbSDKScript();
-    
-    return isInitialized;
-
+  // Load the SDK asynchronously
+  (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  window.fbAsyncInit = () => 
+  {
+      window.FB.init({
+          appId: 281271229627551,
+          cookie: true,
+          xfbml: true,
+          version: 'v6.0'
+      });
+  }
 }
-export default useInitFacebookSDK;
+
+
