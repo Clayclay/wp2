a90136e6b7bc2a3bf203131644d62bf8c6d8ee70
Marie <Mariepolmontiel@gmail.com>
2021-04-30 15:06:44 +0200

    cleaning useinitfcbSDK

--- client/src/hooks/Login/useInitFacebookSDK.js
+++ client/src/hooks/Login/useInitFacebookSDK.js
@@ -1,22 +1,25 @@
-import React , {  useState } from 'react';
+import React from 'react';
 
+
+// Injects the Facebook SDK into the page
 const injectFbSDKScript = () => {
-    ;((d, s, id) => {
-      const element = d.getElementsByTagName(s)[0]
-      const fjs = element
-      let js = element
-      if (d.getElementById(id)) {   return  }
-      js = d.createElement(s)
-      js.id = id
-      js.src = `https://connect.facebook.net/en_US/sdk.js`
-      fjs.parentNode.insertBefore(js, fjs)
-    })(document, 'script', 'facebook-jssdk')
-}
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+};
 
-const useInitFacebookSDK = () => {
+export const useInitFacebookSDK = () => {
 
     const fcbAppId = '281271229627551' // process.env.FACEBOOK_APP_ID
-    const [isInitialized,setIsInitialized]=useState(false)
+    const [isInitialized,setIsInitialized]=React.useState(false)
 
     window.fbAsyncInit = () => {
         window.FB.init({
