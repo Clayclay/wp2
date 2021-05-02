4ab43548b1898fb9007bdae5411a9c7345adaa6e
Marie <Mariepolmontiel@gmail.com>
2021-05-02 17:40:20 +0200

    consolelog car fctionne tjrs pas

--- client/src/hooks/Login/useInitFacebookSDK.js
+++ client/src/hooks/Login/useInitFacebookSDK.js
@@ -18,6 +18,7 @@ const injectFbSDKScript = () => {
 export const useInitFacebookSDK = () => {
   const [isInitialized, setIsInitialized] = React.useState(false);
   const facebookAppId = process.env.FACEBOOK_ID;
+  console.log("appid",process.env.FACEBOOK_ID);
   // Initializes the SDK once the script has been loaded
   // https://developers.facebook.com/docs/javascript/quickstart/#loading
   window.fbAsyncInit = function () {
