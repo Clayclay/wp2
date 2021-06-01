e1c8595b01fbbdcbe5d7bcc30dab487d4c868b2c
Marie <Mariepolmontiel@gmail.com>
2021-06-01 10:45:03 +0200

    essai dans le dernier use effect

--- client/src/hooks/Login/FacebookAccess.js
+++ client/src/hooks/Login/FacebookAccess.js
@@ -8,8 +8,6 @@ import { useHistory } from "react-router-dom";
 import * as ACTION_TYPES from '../../store/actions/action_types';
 import { authContext } from "../../App";
 
-import {getUserMail} from "../../function/GetUserMail";
-
 function deleteCookie(name) {
   document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
@@ -69,35 +67,7 @@ console.log('isfbinitialized',isFbSDKInitialized);
       if (response.authResponse) {
         setFbUserAccessToken(response.authResponse.accessToken)
         window.FB.api('/me', {fields: 'first_name,last_name,email'}, function(response) {
-          setfcbUser(response);
-
-          
-          if (response.email)  {
-          fetch (`/api/fcbuser/${response.email}` ,{ 
-                method: "GET",
-                headers: {
-                  'Content-Type': 'application/json'
-                },
-              })
-              .then(res => {  
-                if (res.ok) {  return res.json(); }
-                  throw res;   
-              })
-              .then(resJson => {
-                console.log("step 3 user register ? ",resJson);  
-                setisRegister(resJson)
-                dispatch({ 
-                      type: ACTION_TYPES.LOGIN_SUCCESS,
-                      payload: resJson
-                })
-              })
-              .catch(error => {
-              console.error(error);
-              setError(error)
-              }); 
-              console.log( 'fcbUser',response  );
-          }else{}
-          
+          setfcbUser(response);        
         })
       }else{console.log('User cancelled login or did not fully authorize.'); }
 
@@ -106,11 +76,38 @@ console.log('isfbinitialized',isFbSDKInitialized);
 },[isFbSDKInitialized,loginState]);
 
     React.useEffect(()=>{
-      console.log('Loginstate', loginState);
-     
 
-      if ( fcbUser && isRegister == undefined  && fcbUser.email !== undefined ){ 
-        console.log("finale step", isRegister , fcbUser.email)
+      console.log('Loginstate', loginState);
+        
+      if (fcbUser.email )  {
+        fetch (`/api/fcbuser/${response.email}` ,{ 
+              method: "GET",
+              headers: {
+                'Content-Type': 'application/json'
+              },
+            })
+            .then(res => {  
+              if (res.ok) {  return res.json(); }
+                throw res;   
+            })
+            .then(resJson => {
+              console.log("step 3 user register ? ",resJson);  
+              setisRegister(resJson)
+              dispatch({ 
+                    type: ACTION_TYPES.LOGIN_SUCCESS,
+                    payload: resJson
+              })
+            })
+            .catch(error => {
+            console.error(error);
+            setError(error)
+            }); 
+            console.log( 'fcbUser',response  );
+        }else{
+
+      console.log("finale step", isRegister ,"email", fcbUser.email)
+      if (  isRegister == undefined   ){ 
+       
         console.log("push ?")
             history.push({  
               pathname:'/fcbRegister' ,
@@ -118,6 +115,7 @@ console.log('isfbinitialized',isFbSDKInitialized);
             })  
 
       } 
+    }
        
     },[fcbUser]);
 
