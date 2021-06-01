948181871d9a78e7f815500923ac0e0f521cf142
Marie <Mariepolmontiel@gmail.com>
2021-06-01 09:45:40 +0200

    problem de &&

--- client/src/hooks/Login/FacebookAccess.js
+++ client/src/hooks/Login/FacebookAccess.js
@@ -64,7 +64,7 @@ const FacebookAccess = ( ) => {
 
 React.useEffect(()=>{
 console.log('isfbinitialized',isFbSDKInitialized);
-  if(isFbSDKInitialized){
+  if(isFbSDKInitialized == true ){
 
     window.FB.getLoginStatus(function(response) {
       setLoginState(response)   
